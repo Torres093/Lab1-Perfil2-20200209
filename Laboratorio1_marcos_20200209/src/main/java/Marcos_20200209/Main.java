7afdda3c21caae750a0f7e546123ac747a4a9366
Torres093 <marcosalejandrot583@gmail.com>
2025-06-06 10:55:44 -0600

    por si se va la luz

--- Laboratorio1_marcos_20200209/src/main/java/Marcos_20200209/Main.java
+++ Laboratorio1_marcos_20200209/src/main/java/Marcos_20200209/Main.java
@@ -1,6 +1,47 @@
 package Marcos_20200209;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Producto demo = new Producto();
+
+        System.out.println("Bienvenido al negocio, Elgige una de las opciones: ");
+        System.out.println("1. Agregar un producto");
+        System.out.println("2. Mostrar los productos");
+        System.out.println("3. Eliminar un producto");
+        System.out.println("4. Salir del sistema");
+
+        Scanner sc = new Scanner(System.in);
+         int eleccion = sc.nextInt();
+
+        switch (eleccion) {
+             case 1:
+                 System.out.println("ingrese el nombre del producto que desea agregar");
+                 Scanner sc2 = new Scanner(System.in);
+                 String nameProducto = sc2.nextLine();
+                 System.out.println("El producto se agregó correctamente");
+                 break;
+             case 2:
+                 System.out.println("Estos Son los productos disponibles: ");
+                 System.out.println("");
+                 break;
+             case 3:
+                 System.out.println("Ingrese el ID del producto que desea eliminar");
+                 Scanner sc3 = new Scanner(System.in);
+                 int idProducto = sc3.nextInt();
+                 break;
+             case 4:
+                System.out.println("Tenga un buen día :)");
+                System.exit(0);
+                break;
+             default:
+                 System.out.println("Porfavor ingrese una opción valida");
+                 break;
+
+
+         }
+
 
     }
 }
---
+++ Laboratorio1_marcos_20200209/src/main/java/Marcos_20200209/Producto.java
@@ -0,0 +1,11 @@
+package Marcos_20200209;
+
+import java.util.TreeMap;
+
+public class Producto extends Main {
+     TreeMap<Integer, String> mapa = new TreeMap<>();
+
+
+     //Añadiendo Productos
+     String nameProducto = "MArcos";
+}
